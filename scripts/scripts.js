bb4b4621dc4703acf955864b1eb9585767193ccf
ADImantra <C.S.Minty@Gmail.com>
2023-07-13 16:49:55 -0700

    Made the randomizer function, turns the screen into a glitch rave

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,32 @@
 
 
-
+//sets HTML items as objects for us to use
 const gridSpace = document.querySelector(`.sketch-pad`)
 const sketchSpace = document.querySelector(`.sketch-space`);
 const startButton = document.querySelector(`.make-a-book`);
 const color = document.getElementById(`color-select`);
-const sizeSelect = document.querySelectorAll(`input[name="size-select"]`)
+const sizeSelect = document.querySelectorAll(`input[name="size-select"]`);
+const rainbow = document.getElementById(`randomizer`);
 
+//triggers the webpage on DOM load in
 document.addEventListener(`DOMContentLoaded`, initialize);
 startButton.addEventListener(`click`, painter)
 
+/*globally tracks whether the mouse is being pressed for use in the
+"painter" function.
+*/
+let mDown = false;
+
 gridSpace.addEventListener(`mousedown`, () => {
     mDown = true;
+    console.log(color.value);
 })
 
 gridSpace.addEventListener(`mouseup` , () => {
     mDown = false;
 })
 
-let mDown = false;
+
 
 //just a set up screen
 function initialize() {
@@ -26,7 +34,7 @@ function initialize() {
     color.value = `black`;
 }
 
-//sets size of 
+//sets size of "canvas"
 function getCanvasSize() {
     for (const sizeButton of sizeSelect) {
         if (sizeButton.checked) {
@@ -70,15 +78,35 @@ function painter() {
     paintbrush effect by allowing the user to "paint" with the 
     mouse held down.
     */
-    squares.forEach(square => square.addEventListener(`mouseover` , () => {
-        if (mDown === true) {
-            console.log(`paintbrush`)
+    squares.forEach(square => square.addEventListener(`mouseover`, () => {
+        if (mDown && rainbow.checked) {
+            gridSpace.addEventListener(`mousemove`, () => {
+                let num = Math.floor(Math.random() * 11);
+                let colorArr = [`#000000`, `#241f31`, `#3d3846`, `#5e5c64`, `#77767b`,
+                            `#9a9996`, `#c0bfbc`, `#c0bfbc`, `#deddda`, `#f6f5f4`,
+                            `#ffffff`];
+                square.style.backgroundColor = colorArr[num];
+            });
+        } else if (mDown && !(rainbow.checked)) {
             square.style.backgroundColor = color.value;
-        }
-
+        } else {
+            //pass
+        };
     }));
 };
 
+/*
+colors
 
+#000000
+#241f31
+#3d3846
+#5e5c64
+#77767b
+#9a9996
+#c0bfbc
+#deddda
+#f6f5f4
+#ffffff
 
-
+*/
